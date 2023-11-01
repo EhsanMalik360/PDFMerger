2b752d74b200ca7ed8b05e4d9fef54ba57419026
EhsanMalik360 <leverify.bi022@gmail.com>
2023-11-01 14:49:14 +0500

    added

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from PyPDF2 import PdfWriter
+from io import BytesIO
+
+st.title('PDF Merger')
+
+uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True, type=['pdf'])
+
+if uploaded_files:
+    if st.button('Merge PDFs'):
+        merger = PdfWriter()
+
+        for pdf in uploaded_files:
+            merger.append(pdf)
+
+        with st.spinner('Merging PDFs...'):
+            pdf_bytes = BytesIO()
+            merger.write(pdf_bytes)
+            pdf_bytes.seek(0)  # reset the cursor to the beginning of the file
+
+        st.success('Merge Complete!')
+        st.download_button(
+            label="Download Merged PDF",
+            data=pdf_bytes,
+            file_name='merged.pdf',
+            mime='application/pdf'
+        )
